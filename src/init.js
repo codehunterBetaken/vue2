09f803e52ae894dac08dc30934a293d6c1822f42
kinou <kinnou_gtalk@gmail.com>
2021-10-02 00:28:22 +0800

    dev 1001 任务5：5.处理render方法

--- src/init.js
+++ src/init.js
@@ -1,9 +1,33 @@
-import { initState } from "./state"
+import {
+  initState
+} from "./state"
+
+import { compileToFunction} from "./compiler/index";
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
     const vm = this
     vm.$options = options
     initState(vm)
+    if (vm.$options.el) {
+      //将数据挂载到模版上
+      vm.$mount(vm.$options.el)
+
+    }
+  }
+  Vue.prototype.$mount = function (el) {
+    const vm = this
+    const options = vm.$options
+    el = document.querySelector(el)
+
+    console.log(el)
+    if(!options.render) { //没有render的情况下用template
+        let template  = options.template
+        if(!template && el) { //没有template的情况下，取el的内容作为模版
+          template = el.outerHTML
+          let render = compileToFunction(template)
+          options.render = render
+        }
+    }
   }
 }

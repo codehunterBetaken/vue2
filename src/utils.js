537079142d2989b9c73edae895aa5ae4511ff4b8
kin ou <wang@example.com>
2021-10-15 15:12:37 +0800

    dev 1015  代码理解

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,12 @@ function timer(flushCallbacks) {
   let timerFn = () => {}
 
   //微任务是在dom渲染前执行的，此处用到的vm.$el是内存内的，并不与dom渲染是否完毕有关
+  //macrotasks: setTimeout ，setInterval， setImmediate，requestAnimationFrame,I/O ，UI渲染
+  //microtasks: Promise， process.nextTick， Object.observe， MutationObserver
+  //1.先执行 macrotasks：I/O -> UI渲染 ->requestAnimationFrame
+  //2.再执行 microtasks ：process.nextTick -> Promise -> MutationObserver -> Object.observe
+  //3.再把setTimeout setInterval setImmediate【三个货不讨喜】 塞入一个新的macrotasks，依次：setTimeout ，setInterval --》setImmediate
+
   if (Promise) {
     timerFn = () => {
       Promise.resolve().then(flushCallbacks)
@@ -26,7 +32,7 @@ function timer(flushCallbacks) {
     let textNode = document.createTextNode(1)
     let observe = new MutationObserver(flushCallbacks)
     observe.observe(textNode, {
-      charaterData: true
+      characterData: true
     })
     timerFn = () => {
       textNode.textContent = 3

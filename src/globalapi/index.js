e124f8b8e1cff72e16ed5823416c83e862dd3772
kinou <kinnou_gtalk@gmail.com>
2021-11-03 00:25:38 +0800

    dev 20211103 任务21：1.生命周期的实现原理

--- src/globalapi/index.js
+++ src/globalapi/index.js
@@ -1,3 +1,10 @@
+import { mergeOptions } from "../utils"
+
 export function initGlobalApi(Vue) {
-  
-}
+  Vue.options = {}
+
+  Vue.mixin = function (options) {
+    this.options = mergeOptions(this.options, options)
+    return this
+  }
+} 
--- src/utils.js
+++ src/utils.js
@@ -61,3 +61,59 @@ export function nextTick(cb) {
     waiting = true
   }
 }
+
+let lifeCycleHooks = [
+  'beforeCreate',
+  'created',
+  'beforeMount',
+  'mounted',
+  'beforeUpdate',
+  'updated',
+  'beforeDestroy',
+  'destroyed'
+]
+
+let strats = {}
+
+function mergeHook(parentVal,childVal) {
+  if(childVal) {
+    if(childVal) {
+        return parentVal.concat(childVal)
+    } else {
+      return [childVal]
+    }
+  }else {
+    return parentVal
+  }
+}
+
+lifeCycleHooks.forEach(hook => {
+  strats[hook] = mergeHook
+})
+
+export function mergeOptions(parent, child) {
+    const options = {}
+    for(let key in parent) {
+      mergeField(key)
+    }
+    for(let key in child) {
+      if(parent.hasOwnProperty(key)) {
+        continue
+      }
+      mergeField(key)
+    }
+
+    function  mergeField(key) {
+      let parentVal = parent[key]
+      let childVal = child[key]
+      if(strats[key]) {
+        options[key] = strats[key](parentVal,childVal)
+      } else {
+        if(isObject(parentVal) && isObject(childVal)) {
+          options[key] = {...parentVal,...childVal}
+        } else {
+          options[key] = child[key]
+        }
+      }
+    }
+}
